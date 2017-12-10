3fb1ed688a2df368004acd5dbe9a6bf1c398afe8
Aaron Hursh <amhursh@gmail.com>
2017-12-09 18:28:31 -0700

    Add things to the project

--- server.js
+++ server.js
@@ -2,19 +2,11 @@ var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
 
-var allowCrossDomain = function(req, res, next) {
-    if ('OPTIONS' == req.method) {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
-
-app.use(allowCrossDomain);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 const environment = process.env.NODE_ENV || 'development'
 const configuration = require('./knexfile')[environment]

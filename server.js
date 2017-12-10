d508c8ebc771e9e0bf8e1872d956eeb60017aae6
Brandon Stewart <boveus@gmail.com>
2017-12-09 17:26:21 -0700

    add index and view for foods API

--- server.js
+++ server.js
@@ -2,40 +2,37 @@ var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
 
+const environment = process.env.NODE_ENV || 'development'
+const configuration = require('./knexfile')[environment]
+const database = require('knex')(configuration)
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.set('port', process.env.PORT || 3000)
 app.locals.title = 'Quantified Self API'
-app.locals.foods = [{"id":1,"name":"Banananana","calories":19},
-{"id":2,"name":"Cheeze Whiz","calories":285},
-{"id":3,"name":"Granola Bar","calories":200},
-{"id":4,"name":"cooler food","calories":1235},
-{"id":5,"name":"GOAT Burrito","calories":10},
-{"id":6,"name":"Bites - Four Bacons ","calories":650},
-{"id":7,"name":"Dan Sandwich","calories":2360},
-{"id":8,"name":"Banh Mimi","calories":342},
-{"id":9,"name":"cool beanz","calories":456},
-{"id":10,"name":"Cheese Curds","calories":600},
-{"id":11,"name":"apples and oranges","calories":129}]
+
 
 app.get('/', function(request, response) {
   response.send(app.locals.title)
 })
 
-app.get('/api/foods', function(request, response) {
-  var notFoods = app.locals.foods
-
-  response.json( notFoods )
+app.get('/api/v1/foods', function(request, response) {
+  database.raw('SELECT * FROM foods')
+    .then((data) => {
+      response.json( data.rows )
+    })
 })
 
-app.get('/api/foods/:id', function(request, response) {
-  var id = request.params.id
-  var message = app.locals.foods[id]
-
-  if (!message) { return response.sendStatus(404)  }
-
-  response.json( id )
+app.get('/api/v1/foods/:id', function(request, response) {
+  database.raw('SELECT * FROM foods WHERE id = (?)', [request.params.id])
+    .then((data) => {
+      if (!data.rows[0]) {
+        return response.sendStatus(404)
+      } else {
+        response.json( data.rows )
+      }
+    })
 })
 
 if (!module.parent) {
